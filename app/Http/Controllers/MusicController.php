74d31aed9dbe75672ef3ba6a0cdece97bcda5d97
Cyraullie <cyril.goldenschue@gmail.com>
2024-04-23 17:48:03 +0200

    finish store music function

--- app/Http/Controllers/MusicController.php
+++ app/Http/Controllers/MusicController.php
@@ -16,7 +16,33 @@ class MusicController extends Controller
 
     public function store(Request $request)
     {
-        return response($request, 200);
+       // return response($request->file('image'), 200);
+
+        
+        if ($imagefile = $request->file('image')) {
+              // Générez un nom de fichier unique
+            $fileName = $request->input('name') . '.' . $imagefile->getClientOriginalExtension(); // Par exemple, timestamp + extension du fichier
+
+            // Déplacez le fichier temporaire vers le répertoire de stockage définitif en utilisant le nouveau nom de fichier
+            $imagefile->move(public_path('musics'), $fileName); // Assurez-vous que 'musics' est le bon répertoire de stockage
+            
+            $musicfile = $request->file('music');
+                // Générez un nom de fichier unique
+            $musicfileName = $request->input('name') . '.' . $musicfile->getClientOriginalExtension(); // Par exemple, timestamp + extension du fichier
+
+            // Déplacez le fichier temporaire vers le répertoire de stockage définitif en utilisant le nouveau nom de fichier
+            $musicfile->move(public_path('musics'), $musicfileName); // Assurez-vous que 'musics' est le bon répertoire de stockage
+            
+            $music = new Music();
+            $music->name = $request->input("name");
+            $music->filename = $musicfileName;
+            $music->save();
+
+            return response('Ok',200);
+        } else {
+            return response('Bad request',400);
+        }
+        
         /*
         try {
             $level = new Level();
